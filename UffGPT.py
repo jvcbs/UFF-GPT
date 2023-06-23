54f2c783f4ce7b411e0be5d623966083d4bc4448
João Victor Costa <costa_joao@id.uff.br>
2023-06-22 23:39:51 -0300

    Versao com Login GOOGLE

--- UffGPT.py
+++ UffGPT.py
@@ -7,6 +7,28 @@ from flask import url_for
 from flask import session
 import flask
 import mysql.connector
+from authlib.integrations.flask_client import OAuth
+
+
+app = Flask(__name__,static_folder='static')
+idlogado = 0
+
+# oAuth Setup
+oauth = OAuth(app)
+google = oauth.register(
+    name='google',
+    client_id= '1070254313928-gro5r8p57k6nje9dr67qga9mpdp7uphs.apps.googleusercontent.com',
+    client_secret='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',
+    access_token_url='https://accounts.google.com/o/oauth2/token',
+    access_token_params=None,
+    authorize_url='https://accounts.google.com/o/oauth2/auth',
+    authorize_params=None,
+    api_base_url='https://www.googleapis.com/oauth2/v1/',
+    client_kwargs={'scope': 'email profile'},
+    server_metadata_url= 'https://accounts.google.com/.well-known/openid-configuration'
+)
+
+
 
 conexao = mysql.connector.connect(
     host = 'localhost',
@@ -17,8 +39,6 @@ conexao = mysql.connector.connect(
 
 cursor = conexao.cursor()
 
-app = Flask(__name__,static_folder='static')
-idlogado = 0
 
 @app.route("/", methods=['GET','POST'])
 def homepage():
@@ -44,7 +64,8 @@ def homepage():
 
 @app.route("/login", methods=['GET','POST'])
 def loginpage():
-    
+
+
     if flask.request.method == 'POST':
     
         user = request.form.get('usuário')
@@ -60,16 +81,23 @@ def loginpage():
         if len(resultado)!=0:
             flash("Olá " + str(resultado[0][1]) + "!\nSeja bem-vindo", "sucesso")
             session['id'] = resultado[0][0]
+            print('homecleitom')
             return redirect(url_for("homepage"))
         
         else:
             flash("Login inválido! Tente novamente...", "erro")
-            return render_template("login.html")
+            print('homecleitom AKI')
+            return redirect(url_for("loginpage"))
     else:
         return render_template("login.html")
 
         
+@app.route("/loginG", methods=['GET','POST'])
+def loginG():
 
+    google = oauth.create_client('google')  # create the google oauth client
+    redirect_uri = url_for('authorize', _external=True)
+    return google.authorize_redirect(redirect_uri)
     
 
 
@@ -111,6 +139,45 @@ def cadastro():
 
         return redirect(url_for("homepage"))
 
+
+@app.route('/logout')
+def logout():
+
+    session.pop('id')
+
+    for key in list(session.keys()):
+        session.pop(key)
+
+    return redirect ('/')
+
+
+@app.route('/authorize')
+def authorize():
+    google = oauth.create_client('google')
+    token = google.authorize_access_token()
+    resp = google.get('userinfo')
+    user_info = resp.json()
+    user = oauth.google.userinfo()
+    session['profile'] = user_info
+    session.permanent = True
+    print(user)
+
+    nome_user = user['name']
+    senha_user = user['sub']
+
+    comando = f'INSERT IGNORE INTO login_db (user_login, user_password) VALUES ("{nome_user}", "{senha_user}") '
+    cursor.execute(comando)
+    conexao.commit()
+
+
+    comando = f"SELECT id_login FROM login_db WHERE user_password = '{senha_user}'"
+    cursor.execute(comando)
+    resultado = cursor.fetchall()
+    session['id'] = resultado[0][0]
+
+    flash("Olá " + str(user['given_name']) + "!\nSeja bem-vindo", "sucesso")
+
+    return redirect('/')
 
 
 if __name__ == "__main__":
